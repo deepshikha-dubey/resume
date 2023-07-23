9f0952180c89f2ee0cc9ccde5098c6b9f55b9168
deepshikha-dubey <140066430+deepshikha-dubey@users.noreply.github.com>
2023-07-23 18:32:50 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php  require_once('layout/include.php'); ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+  <!-- carousel-image-->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000" data-hover="pause">
+     <ol class="carousel-indicators">
+			  <li data-target="#myCarousel" class="active" data-slide-to="0"></li>
+			  <li data-target="#myCarousel" data-slide-to="1"></li>
+			  <li data-target="#myCarousel" data-slide-to="2"></li>
+			  <li data-target="#myCarousel" data-slide-to="3"></li>
+			  <li data-target="#myCarousel" data-slide-to="4"></li>
+			  <li data-target="#myCarousel" data-slide-to="5"></li>
+			  <li data-target="#myCarousel" data-slide-to="6"></li>
+			  <li data-target="#myCarousel" data-slide-to="7"></li>
+			  <li data-target="#myCarousel" data-slide-to="8"></li>
+              <li data-target="#myCarousel" data-slide-to="9"></li>
+              <li data-target="#myCarousel" data-slide-to="10"></li>
+              <li data-target="#myCarousel" data-slide-to="11"></li>
+              <li data-target="#myCarousel" data-slide-to="12"></li>
+
+			</ol>
+<!-- carousel-inner -->
+			<div class="carousel-inner">
+               <div class="item active">
+               	<img src="image/slider.jpg">
+               </div>
+
+                  <div class="item">
+                <img src="image/1.png">
+               </div>
+
+                 <div class="item">
+                <img src="image/slider2.png">
+               </div>
+
+               <div class="item">
+               	<img src="image/slide2.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider3.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider4.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider5.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider6.jpg">
+               </div>
+
+                <div class="item">
+                <img src="image/slider8.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider7.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider1.jpg">
+               </div>
+
+               <div class="item">
+               	<img src="image/slider10.jpg">
+               </div>
+
+               <div class="item">
+                <img src="image/slider9.jpg">
+               </div>
+		    </div>	
+
+		    <a href="#myCarousel" class="carousel-control left" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span></a>
+              <a href="#myCarousel" class="carousel-control right" data-slide="next">
+			  <span class="glyphicon glyphicon-chevron-right"></span></a>	
+<!-- end carousel-inner -->		    	
+    </div>	
+  <!-- end of Carousel-->
+
+  
+  <div class="container">
+  	 	<center><h2>Welcome to world class service Platform</h2></center>
+  	 	<div class="row" style="margin-top:20px;">
+  	 		<div class="col-sm-offset-1 col-sm-5">
+  	 			<P class="text-justify">Welcome to Angel Beauty Salon & Spa. We are one of the pioneers beauty salon & spa in Banglore offering very best and latest treatments for your skin and hair. We are specialized for Bridal Makeup, Skin Treatments, Hair Treatments and other many Beauty service. We pride ourselves on being one of the most renewed beauty salon in providing good service to our clients of Banglore. Angel beauty salon & spa where Mr. One to open the world to beauty. Angel beauty salon & spa has been a pioneer in nurturing talent, they are best professional bridal makeup artist in Banglore. The team is young, with a drive and passion to be the biggest and the best. The team is constantly being trained and inspired. We respect your busy life, and provide flexible appointments for working women. As professional makeup artist in Banglore evolved and brought a whole spectrum of professional hair and skin care services.</P>
+  	 		</div>
+
+  	 		<div class="col-sm-5" style="margin-top: 20px;">
+  	 			<div class="panel panel-danger">
+  	 				<div class="panel-heading"><b>Our Branches</b></div>
+  	 				<div class="panel-body">
+  	 					<table>
+  	 			<tr>
+  	 				<td class="td"><span class="glyphicon glyphicon-home" style="padding-bottom: 40px;"></span></td>
+  	 				<td><p class="text-justfy"> #56 Sri Annapoorneshwari Nilaya, Behind Prashanth Layout, opp. Amrik Hospital, Pai Layout, Hope Farm Whitefield, Bangalore-66</p></td>
+  	 		   </tr>
+
+  	 			<tr>
+  	 				<td class="td"> <span class="glyphicon glyphicon-floppy-disk" ></span></td>
+  	 			    <td><p> Hagadur Main Road, Vinayakanagara, Behind Ganesha Temple, Bangalore-560 066</p></td>
+  	 			</tr>
+  	 			 <tr>
+  	 			    <td class="td"><span class="glyphicon glyphicon-envelope"></span></td>
+  	 		    	<td><p> angelbeautyparlour@gmail.com</p></td>
+  	 			 </tr>
+  	 		    <tr>
+  	 			    <td class="td"><span class="glyphicon glyphicon-earphone"></span></td>
+  	 				<td><p> 9986683097, 9451021340</p></td>
+  	 			 </tr>
+  	 					</table>
+          
+  	 				</div>
+  	 			</div>
+  	 		</div>
+  	    </div>
+  </div>
+  <br>
+</body>
+</html>
+
+<?php require_once('layout/footer.php');
